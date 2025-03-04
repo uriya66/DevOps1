6d0e85e1d32e2e08d6613763efcef4798925bae2
uriya66 <35742868+uriya66@users.noreply.github.com>
2025-03-04 20:58:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Home Page", 200
 
 @app.route('/health')
 def health_check():
     return jsonify({"status": "ok"}), 200
 
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"error": "Not Found"}), 404
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-            
